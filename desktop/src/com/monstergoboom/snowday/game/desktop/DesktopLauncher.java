843e58d743e9203924a3d16f28d347cb274fc197
Alek Mitrevski <amitrevski@godaddy.com>
2014-12-28 18:14:14 -0700

    added destkop and ios projects to solution

---
+++ desktop/src/com/monstergoboom/snowday/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.monstergoboom.snowday.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.monstergoboom.snowday.game.SnowDay;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "SnowDay";
+        config.width = 1024;
+        config.height = 768;
+
+		new LwjglApplication(new SnowDay(), config);
+	}
+}
